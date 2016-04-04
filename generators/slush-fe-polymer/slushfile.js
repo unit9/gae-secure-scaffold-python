70d3956f270c65ca3d88144041f53f8fc55519a7
Krzysztof Nowak <krzysztof.nowak@unit9.com>
2016-04-04 16:30:13 +0200

    Readme updated with info about element generator, generator for new element as part of project generator

--- generators/slush-fe-polymer/slushfile.js
+++ generators/slush-fe-polymer/slushfile.js
@@ -15,14 +15,20 @@ var gulp = require('gulp'),
   _ = require('underscore.string'),
   inquirer = require('inquirer'),
   runSequence = require('run-sequence'),
+  conflict = require('gulp-conflict'),
+  replace = require('gulp-replace'),
   path = require('path'),
+
+
 
   config = {
     paths: {
       dist: './src/app',
       app: '/templates/app',
-      proj: '/templates/proj'
-    }
+      proj: '/templates/proj',
+      tpl: '/templates/element'
+    },
+    appendImport: true
   };
 
 function format(string) {
@@ -114,4 +120,69 @@ gulp.task('default', function (cb) {
       config.answers = answers;
       runSequence('copy-files', ['templatize-app', 'templatize-project-files'], cb);
     });
+});
+
+gulp.task('element', function (cb) {
+  var path, name, split, outputPath, bowerPath, srcPath;
+
+  // Validate arguments.
+  if (gulp.args.length !== 1) {
+    return console.warn('Invalid number of arguments. Usage: slush fe-polymer-element path/element-name');
+  }
+
+  path = gulp.args[0];
+  if ((split = path.lastIndexOf('/')) === -1) {
+    name = path;
+    path = '';
+  } else {
+    name = path.substring(split + 1);
+    path = path.substring(0, split);
+  }
+
+  if (name.indexOf('-') === -1) {
+    return console.warn('Invalid element name: \'' + name + '\'. Elements need to contain a dash (\'-\').');
+  }
+
+  // Resolve paths.
+  path = path + '/' + name;
+  if (path.indexOf('/') === 0) {
+    path = path.substring(1);
+  }
+  outputPath = config.paths.dist + '/elements/' + path;
+  bowerPath = new Array((path.match(/\//g) || []).length + 4).join('../') + 'bower_components';
+  srcPath = new Array((path.match(/\//g) || []).length + 3).join('../');
+  srcPath = srcPath.substring(0, srcPath.length - 1);
+
+  gulp.src(__dirname + config.paths.tpl + '/**/*', {cwd: __dirname, dot: true})
+    .pipe(template({
+      path: path,
+      name: name,
+      bowerPath: bowerPath,
+      srcPath: srcPath
+    }))
+    .pipe(rename(function (path) {
+      if (path.basename === 'element') {
+        path.basename = name;
+      }
+    }))
+    .pipe(conflict(outputPath))
+    .pipe(gulp.dest(outputPath))
+    .on('end', function () {
+      if (config.appendImport) {
+        gulp.src(config.paths.dist + '/elements/elements.jade')
+          .pipe(replace('link(rel=\'import\' href=\'' + path + '/' + name + '.html\')\n', ''))
+          .pipe(replace('// slush:elements', 'link(rel=\'import\', href=\'' + path + '/' + name + '.html\')\n// slush:elements'))
+          .pipe(gulp.dest(config.paths.dist + '/elements/'))
+          .on('end', function () {
+            cb();
+          });
+      } else {
+        console.log('**********************************');
+        console.log('Please append the import manually:');
+        console.log('link(rel=\'import\', href=\'/elements/' + path + '/' + name + '.html\')');
+        console.log('**********************************');
+        cb();
+      }
+    })
+    .resume();
 });
--- generators/slush-fe-polymer/templates/proj/README2.md
+++ generators/slush-fe-polymer/templates/proj/README2.md
@@ -1,4 +1,4 @@
-# Secure GAE Scaffold with Polymer(UNIT9)
+# Secure GAE Scaffold with Polymer (UNIT9)
 
 ## Polymer
 ----
@@ -12,3 +12,11 @@ removed.
 
 Gulp script has been modified to handle framework files and structure. Some 
 additional tasks has been created to handle and optimise final build.
+
+## New Element
+----
+
+To create new element for polymer framework you don't have to create bunch of 
+files manually. Use appropriate generator instead:
+
+`$ slush fe-polymer:element [path/to/][element-name]`
