0c7d670caeec63b05e458ecca386f329e92fdad3
Krzysztof Nowak <krzysztof.nowak@unit9.com>
2016-04-13 15:10:01 +0200

    watcher fixed

--- generators/slush-fe-angular2/templates/proj/tools/utils/seed/watch.ts
+++ generators/slush-fe-angular2/templates/proj/tools/utils/seed/watch.ts
@@ -1,14 +1,14 @@
 import * as runSequence from 'run-sequence';
 import {notifyLiveReload} from '../../utils';
-import * as gulpLoadPlugins from 'gulp-load-plugins';
 import {join} from 'path';
 import {APP_SRC} from '../../config';
-const plugins = <any>gulpLoadPlugins();
+import * as chokidar from 'chokidar';
 
 export function watch(taskname: string) {
   return function () {
-    plugins.watch(join(APP_SRC, '**'), (e:any) =>
-      runSequence(taskname, () => notifyLiveReload(e))
-    );
+    chokidar.watch(join(APP_SRC, '**'),
+      {ignored: /[\/\\]\./, usePolling: true}).on('change', (e:any) => {
+        runSequence(taskname, () => notifyLiveReload(e));
+      });
   };
 }
